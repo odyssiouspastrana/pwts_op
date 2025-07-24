e60ea02fe111812b2c8e5050cdd584c7e1355b4b
ody <geo.odypastrana@gmail.com>
2025-07-24 20:25:41 +0800

    update login test

--- src/tests/ui/login.spec.ts
+++ src/tests/ui/login.spec.ts
@@ -12,10 +12,41 @@ test.describe("Login", () => {
 
 			const username = "admin";
 			const password = "_mywWorld_";
-
 			await loginPage.login(username, password);
 
 			await loginPage.expectHomeLogo();
+		},
+	);
+
+	test(
+		"Login user with invalid credentials",
+		{
+			tag: TAGS.SMOKE,
+		},
+		async ({ loginPage }) => {
+			await loginPage.expectSignInHeadingToBeVisible();
+
+			const username = "uknown_user";
+			const password = "wrong_password";
+			await loginPage.login(username, password);
+
+			await loginPage.expectInvalidCredentialWarning();
+		},
+	);
+
+	test(
+		"Login user with blank credentials",
+		{
+			tag: TAGS.SMOKE,
+		},
+		async ({ loginPage }) => {
+			await loginPage.expectSignInHeadingToBeVisible();
+
+			const username = "";
+			const password = "";
+			await loginPage.login(username, password);
+
+			await loginPage.expectInvalidCredentialWarning();
 		},
 	);
 });
